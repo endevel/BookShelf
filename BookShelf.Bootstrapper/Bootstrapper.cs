244401051f957fee45ce9c3ffbb68826fcf31caf
Alex Bykov <endevel@mail.ru>
2023-08-22 23:08:46 +0500

    Add bootstrapper

---
+++ BookShelf.Bootstrapper/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using BookShelf.Views;
+
+namespace BookShelf.Bootstrapper
+{
+    public class Bootstrapper : IDisposable
+    {
+        public Window Run()
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            return mainWindow;
+        }
+        
+        public void Dispose()
+        {
+        }
+    }
+}
